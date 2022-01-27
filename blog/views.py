20b7bf201c2505856cb84fb776546ee3e99b8e0e
AMANGAUD2000 <aman.gaud2000@gmail.com>
2022-01-28 01:34:58 +0530

    update

--- blog/views.py
+++ blog/views.py
@@ -12,64 +12,66 @@ from django.contrib.auth.models import Group
 # Create your views here.
 
 
-
 def registerPage(request):
 
-	form = CreateUserForm()
-	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			username = form.cleaned_data.get('username')
-			messages.success(request, 'Account was created for ' + username)
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + username)
 
-			return redirect('login')		
+            return redirect('login')
 
-	context = {'form':form}
-	return render(request, 'blog/register.html', context)
+    context = {'form': form}
+    return render(request, 'blog/register.html', context)
 
 
 def loginPage(request):
 
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password =request.POST.get('password')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-		user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username, password=password)
 
-		if user is not None:
-			login(request, user)
-			return redirect('home')
-		else:
-			messages.info(request, 'Username OR password is incorrect')
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
 
-	context = {}
-	return render(request, 'blog/login.html', context)
+    context = {}
+    return render(request, 'blog/login.html', context)
+
 
 def logoutUser(request):
-	logout(request)
-	return redirect('home')
-
-
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
     return render(request, 'blog/home.html')
 
 
+author = User.objects.all()
+
+
 def blogs_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/bloglist.html', {'posts': posts})
+    return render(request, 'blog/bloglist.html', {'bloggers': author, 'posts': posts})
 
 
 def blogger_detail(request, author_id):
     blogger = User.objects.get(id=author_id)
     posts = Post.objects.filter(blogger_name=author_id)
-    return render(request, 'blog/bloggerdetail.html',{'blogger': blogger,'posts':posts})
+    return render(request, 'blog/bloggerdetail.html', {'blogger': blogger, 'posts': posts})
 
 
 def blog_post_detail(request, pk):
     post = Post.objects.get(id=pk)
+
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
